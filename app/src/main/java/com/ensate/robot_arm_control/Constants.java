369314bd58b03cebd9baff21d1cab271fca3b7cb
kowama <n.kowama@gmail.com>
2019-03-10 12:11:16 +0100

    Connect device activity setup
    Signed-off-by: kowama <n.kowama@gmail.com>

---
+++ app/src/main/java/com/ensate/robot_arm_control/Constants.java
@@ -0,0 +1,23 @@
+package com.ensate.robot_arm_control;
+
+/**
+ * Defines several constants used between {@link BluetoothService} and the UI.
+ */
+public class Constants {
+
+    // Message types sent from the BluetoothChatService Handler
+    public static final int MESSAGE_STATE_CHANGE = 1;
+    public static final int MESSAGE_READ = 2;
+    public static final int MESSAGE_WRITE = 3;
+    public static final int MESSAGE_DEVICE_NAME = 4;
+    public static final int MESSAGE_TOAST = 5;
+
+    // Key names received from the BluetoothChatService Handler
+    public static final String DEVICE_NAME = "layout_device_name";
+    public static final String TOAST = "toast";
+    public static final String APP_UUID = "00001101-0000-1000-8000-00805f9b34fb";
+
+    private Constants() {
+
+    }
+}
